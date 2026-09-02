9003874143a33a7679bd630b1b5e55fcd2e75c00
Bsuchot <bastien.suchot@gmail.com>
2026-09-02 15:33:31 +0200

    animation and player movement

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -2,15 +2,34 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+    public float moveSpeed = 3f;
+
+    public Rigidbody2D rigidbody;
+
+    public Animator animator;
+
+    public SpriteRenderer spriteRenderer;
+
+    private Vector2 movement;
 
     // Update is called once per frame
     void Update()
     {
-        
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+
+        movement = movement.normalized;
+
+        animator.SetFloat("Speed", movement.sqrMagnitude);
+
+        if(movement.x != 0)
+        {
+            spriteRenderer.flipX = movement.x < 0;
+        }
+    }
+
+    void FixedUpdate()
+    {
+        rigidbody.linearVelocity = movement * moveSpeed;
     }
 }
